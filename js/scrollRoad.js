e988ef55e95a5ec806c2691ac6f0d52a4a1e6a0d
Hui-Chen <135561949+ws240024@users.noreply.github.com>
2023-10-30 21:05:41 +0800

    rwd update

--- js/scrollRoad.js
+++ js/scrollRoad.js
@@ -1,11 +1,15 @@
 // 宣告路徑
 let path = document.querySelector('.theRoad');
+let straightPath = document.querySelector('.theRoad-s');
 let pathLength = path.getTotalLength();
+let pathLengthStraight = straightPath.getTotalLength();
 // console.log(pathLength);
 path.style.strokeDasharray = pathLength + ' ' + pathLength;
+straightPath.style.strokeDasharray = pathLengthStraight + ' ' + pathLengthStraight;
 
 // 設定速度倍數，调整滾動速度
 let speedMultiplier = 2.5;
+let speedMultiplierStraight = 1.5;
 
 window.addEventListener('scroll', () => {
     // 獲取滾動的位置
@@ -17,8 +21,21 @@ window.addEventListener('scroll', () => {
     var planPosition = plan.offsetTop;
 
     if (scrollPosition >= planPosition) {
-        // 滚动到 plan 时开始绘制路径
+        // 滾動到plan時開始有滾動
         var drawPosition = (scrollPosition - planPosition) * speedMultiplier - (windowHeight / 2);
         path.style.strokeDashoffset = pathLength - drawPosition;
+
+        var drawPositionS = (scrollPosition - planPosition) * speedMultiplierStraight - (windowHeight / 2);
+        straightPath.style.strokeDashoffset = pathLengthStraight - drawPositionS;
     }
 });
+
+//當992px時移除.theRoad-s
+// window.addEventListener("resize", function () {
+//     // 目前現在的大小
+//     const windowWidth = window.innerWidth;
+//     // 尺寸小於992px時移除class
+//     if (windowWidth < 992) {
+//         straightPath.classList.remove("theRoad-s");
+//     }
+// });
